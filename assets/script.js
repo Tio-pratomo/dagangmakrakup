44321c90a8fa333290bac3d8cf91c0f563ca9dd0
tio <sulistiopratomo@gmail.com>
2021-04-19 12:37:49 +0700

    update perhitungan pada javascript

--- assets/script.js
+++ assets/script.js
@@ -28,8 +28,38 @@ main.addEventListener('click', function (event) {
             main.children[3].children[0].children[2].children[2];
         const totalPrice = document.getElementsByClassName('total')[0];
 
-        lontongCalculation.innerHTML = `${lontong.value} x Rp 1.500 = `;
-        totalItemLontong.innerHTML = `${(lontong.value * 1500).toLocaleString(
+        if (lontong.value === '' && risol.value === '' && tahu.value === '') {
+            lontong.value = 0;
+            risol.value = 0;
+            tahu.value = 0;
+        } else if (lontong.value === '' && risol.value === '') {
+            lontong.value = 0;
+            risol.value = 0;
+        } else if (lontong.value === '' && tahu.value === '') {
+            lontong.value = 0;
+            tahu.value = 0;
+        } else if (risol.value === '' && tahu.value === '') {
+            risol.value = 0;
+            tahu.value = 0;
+        } else if (lontong.value === '') {
+            lontong.value = 0;
+        } else if (risol.value === '') {
+            risol.value = 0;
+        } else if (tahu.value === '') {
+            tahu.value = 0;
+        }
+
+        lontongCalculation.innerHTML = `${lontong.value} x Rp 1.500`;
+        totalItemLontong.innerHTML = ` = ${(
+            lontong.value * 1500
+        ).toLocaleString('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            maximumFractionDigits: 0,
+        })}`;
+
+        risolCalculation.innerHTML = `${risol.value} x Rp 2.000`;
+        totalItemRisol.innerHTML = ` = ${(risol.value * 2000).toLocaleString(
             'id-ID',
             {
                 style: 'currency',
@@ -38,18 +68,8 @@ main.addEventListener('click', function (event) {
             }
         )}`;
 
-        risolCalculation.innerHTML = `${risol.value} x Rp 2.000 = `;
-        totalItemRisol.innerHTML = `${(risol.value * 2000).toLocaleString(
-            'id-ID',
-            {
-                style: 'currency',
-                currency: 'IDR',
-                maximumFractionDigits: 0,
-            }
-        )}`;
-
-        tahuCalculation.innerHTML = `${tahu.value} x Rp 1.000 = `;
-        totalItemTahu.innerHTML = `${(tahu.value * 1000).toLocaleString(
+        tahuCalculation.innerHTML = `${tahu.value} x Rp 1.000`;
+        totalItemTahu.innerHTML = ` = ${(tahu.value * 1000).toLocaleString(
             'id-ID',
             {
                 style: 'currency',
